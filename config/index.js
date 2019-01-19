725fc928031747003c456c98898a079d2f193316
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-01-19 21:43:01 +0300

    Use bcryptjs to hash passwords

--- config/index.js
+++ config/index.js
@@ -2,7 +2,6 @@ export default {
   port: 8080,
   jwt: {
     secret: new Buffer.from('Tdygao7Z2UvjbybRd9N0jrdIxQhMHLhfh2IOPfyQ_17j1mF-3r2vi_u8wnIp0Xnjw5', 'base64'),
-    audience: 'uAsgjhJKDHSFFHSDKJFfdsjH5PG3Oq',
   },
   redis: {
     host: process.env.NODE_ENV === 'production' ? 'redis' : 'localhost',
